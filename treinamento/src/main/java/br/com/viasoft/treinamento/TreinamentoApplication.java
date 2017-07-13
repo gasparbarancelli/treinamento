ff2ddd5b061c77b5961975a4513a73f6e4125f87
gasparbarancelli <gasparbarancelli@gmail.com>
2017-07-13 19:15:02 -0300

    Update TreinamentoApplication.java
    update

--- treinamento/src/main/java/br/com/viasoft/treinamento/TreinamentoApplication.java
+++ treinamento/src/main/java/br/com/viasoft/treinamento/TreinamentoApplication.java
@@ -2,12 +2,10 @@ package br.com.viasoft.treinamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Import;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 
 @SpringBootApplication
-@EnableEurekaClient
 @Import({SpringDataRestConfiguration.class})
 public class TreinamentoApplication {
 
